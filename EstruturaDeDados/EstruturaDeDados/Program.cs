28c67e961af811cdc4ff0ed73f510774b5241a22
Juliana <julianafulanetto.senai@gmail.com>
2024-08-21 14:53:15 -0300

    finalizado os ex com list

--- EstruturaDeDados/EstruturaDeDados/Program.cs
+++ EstruturaDeDados/EstruturaDeDados/Program.cs
@@ -2,8 +2,63 @@
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Minha primeira branch!");
-        Console.WriteLine("Trabalhando com Git/GitHub");
-        Console.WriteLine("Mais uma tentativa de trabalhar com Git");
+        // TRABALHADO COM ELEMENTO (LIST)
+        //Criando uma lista 
+        List<string> frutas = new List<string>();
+
+        //Adicionar itens na lista
+        frutas.Add("Morango");
+        frutas.Add("Manga");
+        frutas.Add("Goiaba");
+        frutas.Add("Uva");
+
+        //Imprimindo os itens da lista
+        //Console.WriteLine("Minhas lista de frutas: ");
+        //foreach (string fruta in frutas)
+        //{
+        //    Console.WriteLine(fruta);
+        //}
+        frutas.ForEach(Console.WriteLine);
+
+        Console.WriteLine();
+        //Imprimir elemento na indice especifica
+        Console.WriteLine("Fruta na indice 2: " + frutas[2]);
+
+        //inserir um elemneto no indice especifico
+        frutas.Insert(1, "Maracuja");
+
+        //imprimir a alista novamente 
+        Console.WriteLine();
+
+        //Console.WriteLine("Minhas lista de frutas: ");
+        //foreach (string fruta in frutas)
+        //{
+        //    Console.WriteLine(fruta);
+        //}
+        frutas.ForEach(Console.WriteLine);
+
+        //Alterar um elemento no indice especifico
+        frutas[4] = "Pêra";
+        //Console.WriteLine("Minha  lista de frutas: ");
+        //foreach (string fruta in frutas)
+        //{
+        //    Console.WriteLine(fruta);
+        //}
+        frutas.ForEach(Console.WriteLine);
+        //remover elementos da lista no indice especifico 
+        frutas.RemoveAt(3);
+
+        //Remover elementos pell valor do conteúdo
+        frutas.Remove("Morango");
+
+        //Imprimindo a lista novamente 
+        Console.WriteLine();
+        //Console.WriteLine("Minha lista de frutas: ");
+        //foreach (string fruta in frutas)
+        //{
+        //    Console.WriteLine(fruta);
+        //}
+        frutas.ForEach(Console.WriteLine);
+
     }
 }
